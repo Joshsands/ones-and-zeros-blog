34c91ba32a1b2a5fd9253a4d72b1cb49d7daddd3
Joshua Sands <josh.sands@mail.com>
2021-08-15 20:21:46 -0500

    heroku push try 7

--- config/connection.js
+++ config/connection.js
@@ -14,32 +14,22 @@
 //   });
 // }
 
-// Set up MySQL connection.
-var mysql = require("mysql");
+const Sequelize = require('sequelize');
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "*Km#a+*)8uuqG}3",
-    database: "ones_and_zeros_db"
-  });
-}
+require('dotenv').config();
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+        logging: false
+    });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+module.exports = sequelize;
+
 
 
 // module.exports = sequelize;
