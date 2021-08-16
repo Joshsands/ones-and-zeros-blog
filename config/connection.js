9fc0b46dc5c10d7914ed30d52b30fec03b2cc8d0
Joshua Sands <josh.sands@mail.com>
2021-08-15 20:13:49 -0500

    added variables back in

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,29 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-let sequelize;
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+// }
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "*Km#a+*)8uuqG}3",
+    database: "ones_and_zeros_db"
   });
 }
+
 
 module.exports = sequelize;
