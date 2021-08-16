f06532f668d36c9bdd2191212b568cb610a9ca72
Joshua Sands <josh.sands@mail.com>
2021-08-15 20:28:27 -0500

    heroku push try again

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: process.env.secret,
   // can add options to the cookie brackets like maximum age.
   cookie: {},
   resave: false,
